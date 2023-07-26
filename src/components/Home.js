a915760c4580794486164c629db67764df9b0efc
Rohan Mishra <rohanmishra.004@gmail.com>
2023-07-26 21:49:29 +0530

    Use Effect Example added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+    const [name, setName] = useState('mario')
     const [blogs, setBlogs] = useState(
         [
             {
@@ -38,11 +39,21 @@ const Home = () => {
         setBlogs(newBlogs)
     }
 
+    //UseEffect mainly used to fetch data or do some authentication which is known as sideEffect in ReactJs
+
+    //Dependencies [] prevent useEffect to run at every render , it only runs when the dependency which is declared inside the useEffect change
+
+    //if we add the name dependency here then useEffect will only run when name value changes and not at any other moment
+    useEffect(() => {
+        console.log('UseEffect triggerd');
+        console.log(name)
+    },[name])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs } title={"All Blogs"} handleDeleteBlog={handleDeleteBlog} />
-           
+            <button onClick={() => setName('luigi')}>Change name</button>
+            <p>{name }</p>
         </div>
      );
 }
