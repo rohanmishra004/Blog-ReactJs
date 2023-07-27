c17ae34da7f2d50c8c1c3b1223e44cf3412aaed4
Rohan Mishra <rohanmishra.004@gmail.com>
2023-07-28 02:18:03 +0530

    Error messages added for both server as well as network issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,27 +2,37 @@ import { useState , useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+    const [blogs,setBlogs] = useState(null);
+    //generally the data that we are fetching from server takes time so we can add additional state to showcase loading
+    const [isPending, setPending] = useState(true)
+    const [error , setError] = useState(null)
 
-
-
-
-    //here we are fetching the data with the fetch call and updating the blogs with setBlogs with the data we get in response
     useEffect(() => {
         fetch("http://localhost:8000/blogs")
-            .then(response => {
+            .then(response => { 
+                console.log(response)
+                if (!response.ok) {
+                    throw Error("could not fetch data for the resource")
+                }
                 return response.json()
             }).then((data) => {
                 console.log(data)
                 setBlogs(data)
-            });
+                
+                //once we have the data we want the loading message to disappear so we can use setPenidng useState to false the pending state
+                setPending(false)
+                setError(null)
+            }).catch((err) => {
+            //Handling error - This will catch anytype of network error
+                setError(err.message)
+                setPending(false)
+            })
     },[])
 
     return ( 
         <div className="home">
-            {/* here blogs is coming from fetch call which takes some time to reach therefore the initial value of blogs is null hence we have added the && to check blogs data and then update */}
-
-            {/* This is called contiondal templating , thereform Logical && evaluates left first and if its null/false it will not process the right side  */}
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title={"All Blogs"}  />}
         </div>
      );
